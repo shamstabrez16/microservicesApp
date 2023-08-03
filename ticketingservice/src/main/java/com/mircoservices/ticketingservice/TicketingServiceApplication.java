810559c351166c3dbc9c1152a3db5c8da2aba2e2
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-08-02 23:49:31 -0500

    Docker-compose file updated to build containers as multi container
    Docker-compose file updated to build containers as multi container

--- ticketingservice/src/main/java/com/mircoservices/ticketingservice/TicketingServiceApplication.java
+++ ticketingservice/src/main/java/com/mircoservices/ticketingservice/TicketingServiceApplication.java
@@ -11,7 +11,14 @@ import org.springframework.retry.annotation.EnableRetry;
 @EnableDiscoveryClient
 @EnableRetry
 public class TicketingServiceApplication {
+/*
+docker run -d -p 8761:8761 --name eureka-server my-eureka-server
 
+docker run  -d -p 8081:8081 --name ticketing-service ticketing-service
+docker run -d -p 8082:8082 --name inventory-system inventory-system
+docker run -d -p 80:80 --name my-nginx-gateway my-nginx-gateway
+create a docker-compose.yml  file
+ */
 	public static void main(String[] args) {
 		SpringApplication.run(TicketingServiceApplication.class, args);
 	}
